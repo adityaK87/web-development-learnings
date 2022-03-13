6be80eb2236b09589727f9a74139fab6d78c2030
Aditya <adityapal8763244@gmail.com>
2022-03-13 10:55:13 +0530

    add tutorial files

---
+++ tut53.js
@@ -0,0 +1,16 @@
+console.log("This is tutorial 53");
+
+function greet(name, greettext="greeting from JavaScript"){
+    console.log(greettext +" "+ name)
+    console.log(name+" is a good boy")
+};
+let name1="Aditya ";
+let name2="Abhirudra ";
+let name3="Aman";
+let greettext="Good Morning"
+// console.log(name1+" is a good boy");
+// console.log(name2+" is a good boy");
+// console.log(name3+" is a good boy");
+greet(name1,greettext);
+greet(name2,greettext) ;
+greet(name3, );
