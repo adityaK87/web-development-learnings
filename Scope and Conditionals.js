fb5ff533f733860efd99d670dc91c4c97a69d4ca
Aditya kumar <adityapal8763244@gmail.com>
2022-09-01 12:40:33 +0530

    Modify files Name For more clarity

---
+++ Scope and Conditionals.js
@@ -0,0 +1,51 @@
+// var String1 = 'This is our String1';
+// var String1 = 'This is our String2';
+// let a = 'u'
+// {
+//      let a = 'u5'
+// console.log(a)
+// }
+
+// console.log(String1);
+
+// const a='This can not be changed';
+// a='abinash'
+// console.log(a);
+
+
+// let age=3;
+// if(age>18){
+//     console.log('you can  Drink Alcohol');
+// }
+
+// else if(age == 3){
+//     console.log('you can drink both');
+// }
+// else if(age == 5){
+//     console.log('you can drink both');
+// }
+// else{
+//     console.log('you can Drink cold drink only')
+// }
+
+const cups = 45;
+switch (cups) {
+    case  4:
+        console.log('The value of cups is 4' )
+        
+        break;
+
+    case  42:
+        console.log('The value of cups is 42' )
+        
+        break;
+
+    case  44:
+        console.log('The value of cups is 44' )
+        
+        break;
+
+    default:
+        console.log("The value of cups is none of 4, 42 ,43 ");
+        break;
+}
